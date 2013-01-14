d3879808eeb20d0afaca528532899d904d67e4ef
Juri <juri.strumpflohner@gmail.com>
2013-01-14 17:34:21 +0100

    some initial test setup for verifying the correct functioning of the backend api

---
+++ WebApi/WebApiBackend/Controllers/PersonController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiBackend.Models;
+
+namespace WebApiBackend.Controllers
+{
+    public class PersonController : ApiController
+    {
+
+        public IEnumerable<Person> GetAll()
+        {
+            return new List<Person>{
+                new Person(){
+                    Id = 1,
+                    Firstname = "Juri",
+                    Lastname = "Strumpflohner"
+                },
+                new Person(){
+                    Id = 2,
+                    Firstname = "Steffi",
+                    Lastname = "Franchi"
+                },
+                new Person(){
+                    Id = 2,
+                    Firstname = "Steffi",
+                    Lastname = "Franchi"
+                }
+            };
+        }
+
+        public Person GetById(int id)
+        {
+            return new Person()
+            {
+                Id = id,
+                Firstname = "Juri"
+            };
+        }
+
+        [HttpPost]
+        public Person SavePerson(Person person)
+        {
+            person.Id = 123;
+            return person;
+        }
+
+    }
+
+}
--- WebApi/WebApiBackend/tests/spec/backendSpecs.js
+++ WebApi/WebApiBackend/tests/spec/backendSpecs.js
@@ -1,14 +1,70 @@
-describe("The Backend", function() {
-
+describe('The Backend', function() {
+    
     beforeEach(function(){
-        
     });
 
     afterEach(function(){
     });
 
-    it("should run the test", function(){
-        
+    it('should return a list of people when invoking /api/person', function(){
+        var fetchedResult;
+
+        $.getJSON('/api/person', function(result){
+            fetchedResult = result;
+        });
+
+        waitsFor(function(){
+            return typeof(fetchedResult) !== 'undefined';
+        }, 1000);
+
+        runs(function(){
+            expect(fetchedResult.length).toBe(3);
+        });
+    });
+
+    it('should return a single person when invoking /api/person/4', function(){
+        var fetchedResult;
+
+        $.getJSON('/api/person/4', function(result){
+            fetchedResult = result;
+        });
+
+        waitsFor(function(){
+            return typeof(fetchedResult) !== 'undefined';
+        }, 1000);
+
+        runs(function(){
+            expect(fetchedResult.firstname).toMatch('Juri');
+            expect(fetchedResult.id).toMatch(4);
+        });
+    });
+
+    it('should return a single person with a new id when executing a POST /api/person', function(){
+        var fetchedResult;
+
+        $.ajax({
+            url: '/api/person',
+            type: 'POST',
+            contentType: 'application/json',
+            dataType: 'json',
+            data: { firstname: 'Juri' },
+            success: function(result){
+                fetchedResult = result;
+            },
+            error: function(error){
+                var exception = JSON.parse(error.responseText);
+                throw exception.ExceptionMessage;
+            }
+        });
+
+        waitsFor(function(){
+            return typeof(fetchedResult) !== 'undefined';
+        }, 1000);
+
+        runs(function(){
+            expect(fetchedResult.id).toMatch(1234);
+            expect(fetchedResult.firstname).toMatch('Juri');
+        });
     });
 
 });
