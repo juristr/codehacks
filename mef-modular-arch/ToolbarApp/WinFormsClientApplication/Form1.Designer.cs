509482a4a3d51af0d81f04ef2a08e05725acac5c
Juri <juri.strumpflohner@gmail.com>
2013-03-04 11:52:16 +0100

    created winforms app

---
+++ mef-modular-arch/ToolbarApp/WinFormsClientApplication/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WinFormsClientApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.applicationMenuBar = new System.Windows.Forms.MenuStrip();
+            this.SuspendLayout();
+            // 
+            // applicationMenuBar
+            // 
+            this.applicationMenuBar.Location = new System.Drawing.Point(0, 0);
+            this.applicationMenuBar.Name = "applicationMenuBar";
+            this.applicationMenuBar.Size = new System.Drawing.Size(741, 24);
+            this.applicationMenuBar.TabIndex = 0;
+            this.applicationMenuBar.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 410);
+            this.Controls.Add(this.applicationMenuBar);
+            this.MainMenuStrip = this.applicationMenuBar;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip applicationMenuBar;
+    }
+}
+
