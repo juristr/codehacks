e227d67f7924c2abf2f221c89be6d71c0909d80b
Juri <juri.strumpflohner@gmail.com>
2013-07-01 09:09:14 +0200

    adding ex 4

--- DotNetHacks/LunchtimeLinq/LunchtimeLinq/LunchtimeLinq.cs
+++ DotNetHacks/LunchtimeLinq/LunchtimeLinq/LunchtimeLinq.cs
@@ -58,6 +58,24 @@ namespace LunchtimeLinq
             Assert.AreEqual(28, new DateTime(1985, 1, 1).GetAge());
         }
 
+        /*
+         *  Exercise 4
+         *  -------------
+         *  Create an enumerable sequence of strings in the form "x,y" 
+         *  representing all the points on a 3x3 grid. e.g. output would be: 
+         *  0,0 0,1 0,2 1,0 1,1 1,2 2,0 2,1 2,2
+         */
+        [TestMethod]
+        public void Ex4_ShouldGenerateA3x3Grid()
+        {
+            var result =
+                Enumerable.Range(0, 3)
+                    .SelectMany(x=> Enumerable.Range(0,3)
+                                        .Select(y => String.Format("{0},{1}", x,y)));
+            Assert.AreEqual("0,0 0,1 0,2 1,0 1,1 1,2 2,0 2,1 2,2", String.Join(" ", result));
+        }
+
+
     }
 
     public static class AgeDateExtension
