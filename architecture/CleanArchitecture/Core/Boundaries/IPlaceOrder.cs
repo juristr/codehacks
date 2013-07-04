8d43d247ba420ad4303cbaefc3a2582254a237e9
Juri <juri.strumpflohner@gmail.com>
2013-07-04 09:45:55 +0200

    adding boundary

---
+++ architecture/CleanArchitecture/Core/Boundaries/IPlaceOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Transversal;
+
+namespace Core.Boundaries
+{
+    public interface IPlaceOrder
+    {
+
+        OrderResultModel PlaceOrder(OrderRequestModel orderRequestModel);
+
+    }
+}
